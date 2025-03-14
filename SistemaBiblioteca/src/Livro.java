b77bec926110b80bdb8763366e9cc5b25a8c0135
Pedro Lucas <plucaslopes391@gmail.com>
2025-03-13 23:20:17 -0300

    Criando métodos para verificação de disponibilidade de do livro

--- SistemaBiblioteca/src/Livro.java
+++ SistemaBiblioteca/src/Livro.java
@@ -1,2 +1,37 @@
 public class Livro {
+
+    //ATRIBUTOS DA CLASSE//
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+    public Livro (String titulo, String autor){ //CONSTRUTOR E PARAMETROS
+        this.titulo = titulo; //"THIS" REFERENCIA A VARIAVEL GLOGAL//
+        this.autor = autor;
+        this.disponivel = true; //JÁ INICIACIZANDO A VARIÁVEL BOOLEANA
+    }
+    //GETTERS & SETTERS
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 }
--- SistemaBiblioteca/src/Usuario.java
+++ SistemaBiblioteca/src/Usuario.java
@@ -1,2 +1,35 @@
 public class Usuario {
+
+    private String nome;
+    private Livro livroAlugado; //USANDO CLASSE "LIVRO" COMO TIPO PARA A VARIÁVEL
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Livro getLivroAlugado() {
+        return livroAlugado;
+    }
+
+    public void setLivroAlugado(Livro livroAlugado) {
+        this.livroAlugado = livroAlugado;
+    }
+
+    public Usuario(String nome){
+        this.nome = nome;
+        this.livroAlugado = null;
+    }
+
+    public void alugarLivro (Livro livro){
+        if (livro.isDisponivel()){ //VERIFICA SE O LIVRO ESTA DISPONÍEVÇ "TRUE"
+            livroAlugado = livro; // SE ESTIVER "TRUE", VAI INICIALIZAR A VARIAÁVEL
+            livro.setDisponivel(false); //VAI TORNAR O "FALSE" POR QUE O LIVRO NÃO ESTÁ MAIS DISPONÍEL, POIS JÁ FOI ALUGADO
+            System.out.println("O usuário: " +nome+ "alugou o livro: "+ livro.getTitulo());
+        } else {
+            System.out.println("O livro: " + livro.getTitulo() + "não está mais disponível"); //SE NÃO ESTIVER DISPONÍEÇ "TRUE"
+        }
+    }
 }
