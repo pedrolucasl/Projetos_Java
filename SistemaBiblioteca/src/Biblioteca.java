5abae9626a0718f5ff5d5e3780561970a79bf756
Pedro Lucas <plucaslopes391@gmail.com>
2025-03-17 21:46:47 -0300

    Implementando as funções de adicionar usuaário e livro; Implementando funções de buscar usuário e livro

--- SistemaBiblioteca/src/Biblioteca.java
+++ SistemaBiblioteca/src/Biblioteca.java
@@ -1,2 +1,57 @@
+import java.util.ArrayList;
+
 public class Biblioteca {
-}
+
+    private ArrayList<Usuario> usuarios; /*CRIANDO VARIÁVEIS DO TIPO DO ARRAY POIS VAI COMPORTAR UM VOLUEM DE INFORMAÇÕES */
+    private ArrayList<Livro> livros;
+
+    public Biblioteca (){ /* CONSTRUTOR */
+        livros = new ArrayList<Livro>(); 
+        usuarios = new ArrayList<Usuario>();
+    }
+
+    public ArrayList<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(ArrayList<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public ArrayList<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(ArrayList<Livro> livros) {
+        this.livros = livros;
+    }
+
+    public void adicionarLivro (Livro livro){
+        livros.add(livro);
+        System.out.println("Livro adicionado: " +livro.getTitulo());
+    }
+
+    public void adicionarUsuario (Usuario usuario){
+        usuarios.add(usuario);
+        System.out.println("Usuário adicionado: " +usuario.getNome());
+    }
+
+    public Livro buscarLivro(String titulo){
+        for (Livro livro: livros){
+            if (livro.getTitulo().equals(titulo)) {
+                return livro;
+            } 
+        } return null;
+    }
+    public Usuario buscarUsuario(String nome){
+        for(Usuario usuario: usuarios){
+            if (usuario.getNome().equals(nome)) {
+                return usuario;
+            }
+        } return null;
+    }
+
+    }
+
+    
+
--- SistemaBiblioteca/src/Usuario.java
+++ SistemaBiblioteca/src/Usuario.java
@@ -2,6 +2,7 @@ public class Usuario {
 
     private String nome;
     private Livro livroAlugado; //USANDO CLASSE "LIVRO" COMO TIPO PARA A VARIÁVEL
+
     public String getNome() {
         return nome;
     }
@@ -24,12 +25,12 @@ public class Usuario {
     }
 
     public void alugarLivro (Livro livro){
-        if (livro.isDisponivel()){ //VERIFICA SE O LIVRO ESTA DISPONÍEVÇ "TRUE"
+        if (livro.isDisponivel()){ //VERIFICA SE O LIVRO ESTA DISPONÍVEL "TRUE"
             livroAlugado = livro; // SE ESTIVER "TRUE", VAI INICIALIZAR A VARIAÁVEL
             livro.setDisponivel(false); //VAI TORNAR O "FALSE" POR QUE O LIVRO NÃO ESTÁ MAIS DISPONÍEL, POIS JÁ FOI ALUGADO
             System.out.println("O usuário: " +nome+ "alugou o livro: "+ livro.getTitulo());
         } else {
-            System.out.println("O livro: " + livro.getTitulo() + "não está mais disponível"); //SE NÃO ESTIVER DISPONÍEÇ "TRUE"
+            System.out.println("O livro: " + livro.getTitulo() + "não está mais disponível"); //SE NÃO ESTIVER DISPONÍEl "TRUE"
         }
     }
 
